b28bb5ba3c8662f991b94a318c17db50f939b2c4
youngtae88.kim <youngtae88.kim@lge.com>
2024-03-29 12:27:49 +0900

    increased readability

--- piccoloyaml/src/file_checker.rs
+++ piccoloyaml/src/file_checker.rs
@@ -1,7 +1,14 @@
+use std::ffi::OsStr;
 use std::fs;
 use std::fs::File;
 use std::io::{Error, ErrorKind, Result, Write};
 use std::path::Path;
+
+macro_rules! Err {
+    ($x:expr) => {
+        Err(Error::new(ErrorKind::Other, $x))
+    };
+}
 
 const SYSTEMD_FILE_PATH: &str = r#"/etc/containers/systemd/"#;
 const CONTENTS_HEADER: &str = r#"[Unit]
@@ -15,23 +22,21 @@ WantedBy=multi-user.target default.target
 [Kube]
 Yaml="#;
 
-fn check_file_extension(name: &str) -> Result<()> {
-    match Path::new(name).extension() {
-        Some(ext) => {
-            if "yaml" == ext {
-                Ok(())
-            } else {
-                Err(Error::new(
-                    ErrorKind::Other,
-                    "Only 'yaml' file extension is allowed.",
-                ))
-            }
-        }
-        _ => Err(Error::new(ErrorKind::Other, "There is no file extension.")),
+fn check_src_file(name: &str) -> Result<String> {
+    let file = Path::new(name);
+    if !file.is_file() {
+        return Err!(format!("Not found or invalid - {}", name));
+    }
+
+    if matches!(file.extension(), Some(x) if x == OsStr::new("yaml")) {
+        let absolute_path = fs::canonicalize(file)?;
+        Ok(absolute_path.into_os_string().into_string().unwrap())
+    } else {
+        Err!("Unsupported file extension")
     }
 }
 
-fn create_dot_kube_file(name: &str) -> Result<()> {
+fn create_dst_file(name: &str) -> Result<()> {
     let kube_file_path = Path::new(name).with_extension("kube");
     let mut file = File::create(kube_file_path)?;
     file.write_all(format!("{}{}", CONTENTS_HEADER, name).as_bytes())?;
@@ -43,25 +48,17 @@ fn create_dot_kube_file(name: &str) -> Result<()> {
 ///                  /etc/containers/systemd/my_pod.kube
 pub fn process(cmd: &str, file_path: &str) -> Result<()> {
     if cmd == "apply" {
-        let src_pathbuf = fs::canonicalize(file_path)?;
-        let src = src_pathbuf.into_os_string().into_string().unwrap();
+        let src = check_src_file(file_path)?;
         let file_name = Path::new(&src).file_name().unwrap().to_str().unwrap();
         let dst = format!("{}{}", SYSTEMD_FILE_PATH, file_name);
 
-        check_file_extension(file_name)?;
         fs::copy(&src, &dst)?;
+        create_dst_file(&dst)?;
 
-        create_dot_kube_file(&dst)?;
         Ok(())
     } else if cmd == "delete" {
-        Err(Error::new(
-            ErrorKind::Other,
-            "Currently 'delete' is not support",
-        ))
+        Err!("Currently 'delete' is not support")
     } else {
-        Err(Error::new(
-            ErrorKind::Other,
-            format!("{} is not support", cmd),
-        ))
+        Err!(format!("{} is not support", cmd))
     }
 }
