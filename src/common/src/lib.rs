e0aa645cd9853ad7f812ccf8218e161a44dd9a4c
youngtae88.kim <youngtae88.kim@lge.com>
2024-08-07 18:37:18 +0900

    change document registry address

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -30,7 +30,7 @@ fn init_conf() -> config::Config {
                 .unwrap()
                 .set_default(
                     "DOC_REGISTRY",
-                    "http://mod.lge.com/hub/piccolo/piccolo-utils/documents-registry/-/raw/main",
+                    "http://mod.lge.com/hub/piccolo/piccolo-bluechi/-/raw/main/examples/res",
                 )
                 .unwrap()
                 .build()
