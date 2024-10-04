27b7e1ab3089fbca0ac7ac2f8731866ee9bd4334
yonghyeon.kim <yonghyeon.kim@lge.com>
2024-10-04 15:48:01 +0900

    [piccolo][ocx]fixed typing error
    fixed typing error

--- src/vendor/dds/lge/src/batterycapacity.rs
+++ src/vendor/dds/lge/src/batterycapacity.rs
@@ -1,6 +1,6 @@
 // TOPIC NAME = rt/piccolo/Battery_Capacity
 #[allow(non_snake_case)]
-pub mod BatteryCiapacity{
+pub mod BatteryCapacity{
     #[derive(Debug, dust_dds::topic_definition::type_support::DdsType)]
     pub struct BatteryCapacityMsg {
         pub capacity:String,
